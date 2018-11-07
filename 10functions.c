8470673b88f977c6162cfaffbc8dbc28ffe039c2
elucodewin <elucodewin@totalwin.de>
2018-11-07 17:29:23 +0100

    10 Funktionen

---
+++ 10functions.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// functions
+void firstfunction(){
+  printf("firstfunction is called\n");
+}
+
+int main(){
+  printf("Calling firstfunction\n");
+  firstfunction();
+  return 0;
+}
