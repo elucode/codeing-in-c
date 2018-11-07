517e652e9525886c7621df8c4a8087f2da22dfb4
elucodewin <elucodewin@totalwin.de>
2018-11-07 15:30:52 +0100

    5te Aufgabe De- und Incremtierung

---
+++ 5deandincrement.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+  int number = 3;
+  number++; // Is equal number = number + 1;
+  printf("True if 4 = %d\n", number);
+
+  number--; // Is equal number = number -1;
+  printf("True if 3 = %d\n", number);
+  return 0;
+}
