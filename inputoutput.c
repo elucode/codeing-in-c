cf0112653626dc4de4fa411628dbfa340107e5e3
elucodelinux <elucodelinux@uneverknow.de>
2018-11-07 11:08:05 +0100

    funktioniert in linux

--- inputoutput.c
+++ inputoutput.c
@@ -3,8 +3,11 @@
 
 int main()
 {
-printf("Wie alt sind sie?");
+int alter;
+printf("Wie alt sind Sie?");
 scanf("%d", &alter);
 printf("\nIn %d Jahren sind Sie 100!\n", 100-alter);
+printf("Wie groß sind Sie?");
+scanf("%d", &alter);
 return 0;
 }
